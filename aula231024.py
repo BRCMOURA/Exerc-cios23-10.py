5a8eff10ce56e13a35cd9b5c7793a8fc49232784
BRCMOURA <brcmoura@gmail.com>
2024-10-23 21:14:25 -0300

    Add files via upload

---
+++ aula231024.py
@@ -0,0 +1,41 @@
+# 1) Faça um programa que leia dados do usuário (nome, sobrenome, idade) adicione em uma lista e imprima seus elementos na tela.
+
+# nome = input('nome: ')
+# sobrenome = input('sobrenome: ')
+# idade = input('idade: ')
+
+# lista = [nome, sobrenome, idade]
+
+# print(lista[0])
+
+# 2) Faça um programa onde o usuário informa um número de 1 a 12 em seguida imprima qual o mês que se refere.
+
+# n = int(input('Digite um número de 1 a 12: '))
+
+# lista_mes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+# n = n - 1
+
+# print(lista_mes[n])
+
+# Extra) Faça um menu onde o usuário consiga selecionar qual questão quer executar.
+
+# questao = int(input('Digite o número 1 ou 2: '))
+
+# if questao == 1:
+#     nome = input('nome: ')
+#     sobrenome = input('sobrenome: ')
+#     idade = input('idade: ')
+
+#     lista = [nome, sobrenome, idade]
+
+#     print(lista[0])
+    
+# else:
+#     n = int(input('Digite um número de 1 a 12: '))
+
+#     lista_mes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+#     n = n - 1
+
+#     print(lista_mes[n])
